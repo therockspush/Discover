97432b346fc57edac11f4cb91270c5bf302c0e31
therockspush <therockspush@users.noreply.github.com>
2021-03-22 18:11:24 -0700

    Update main.tf

--- pan_firewalls/main.tf
+++ pan_firewalls/main.tf
@@ -1,5 +1,3 @@
-
-
 resource "aviatrix_firewall_instance" "firewall_instance" {
   for_each = var.firewall_names
 
@@ -16,9 +14,11 @@ resource "aviatrix_firewall_instance" "firewall_instance" {
   #key_name               = var.key_name == "" ? null : var.key_name
   username               = var.cloud == "azure" ? "avtx1234" : null
   password               = var.cloud == "azure" ? "" : null
+
+  depends_on = [var.vpc_id]
 }
 
-/*
+
 resource "aviatrix_firewall_instance" "firewall_instance_ha" {
   for_each = var.firewall_names
 
@@ -38,11 +38,13 @@ resource "aviatrix_firewall_instance" "firewall_instance_ha" {
 
   depends_on = [aviatrix_firewall_instance.firewall_instance]
 }
-*/
+
 resource "aviatrix_firenet" "firewall_net" {
   vpc_id             = var.vpc_id
   inspection_enabled = var.inspection_enabled
-  egress_enabled     = var.egress_enabled  
+  egress_enabled     = var.egress_enabled
+
+  depends_on = [var.vpc_id]  
 }
 
 resource "aviatrix_firewall_instance_association" "firewall_instance_association_1" {
@@ -56,11 +58,11 @@ resource "aviatrix_firewall_instance_association" "firewall_instance_association
   lan_interface        = aviatrix_firewall_instance.firewall_instance[each.key].lan_interface
   management_interface = var.cloud == "aws" ? aviatrix_firewall_instance.firewall_instance[each.key].management_interface : null
   egress_interface     = aviatrix_firewall_instance.firewall_instance[each.key].egress_interface
-  
+
   depends_on = [aviatrix_firenet.firewall_net]
 }
 
-/*
+
 resource "aviatrix_firewall_instance_association" "firewall_instance_association_2" {
   for_each = var.firewall_names
   vpc_id               = var.vpc_id
@@ -72,116 +74,6 @@ resource "aviatrix_firewall_instance_association" "firewall_instance_association
   lan_interface        = aviatrix_firewall_instance.firewall_instance_ha[each.key].lan_interface
   management_interface = var.cloud == "aws" ? aviatrix_firewall_instance.firewall_instance_ha[each.key].management_interface : null
   egress_interface     = aviatrix_firewall_instance.firewall_instance_ha[each.key].egress_interface
+
+  depends_on = [aviatrix_firenet.firewall_net]
 }
-*/
-
-
-# Vendor integration delays every TF operation significantly
-# Uncomment the following section after the firewalls are available through their UI
-# run terraform again and comment it back. Or configure vendor integration from the UI
-
-/*
-data "aviatrix_firenet_vendor_integration" "pan" {
-  for_each = var.firewall_names
-
-  vpc_id            = var.vpc_id
-  instance_id       = aviatrix_firewall_instance.firewall_instance[each.value].instance_id
-  public_ip         = aviatrix_firewall_instance.firewall_instance[each.value].public_ip
-  vendor_type       = "Palo Alto Networks VM-Series"
-  username          = "admin"
-  password          = "Aviatrix123#"
-  save              = true
-  number_of_retries = 20
-  retry_interval    = 60
-}
-
-data "aviatrix_firenet_vendor_integration" "pan_ha" {
-  for_each = var.firewall_names
-
-  vpc_id            = var.vpc_id
-  instance_id       = aviatrix_firewall_instance.firewall_instance_ha[each.value].instance_id
-  public_ip         = aviatrix_firewall_instance.firewall_instance_ha[each.value].public_ip
-  vendor_type       = "Palo Alto Networks VM-Series"
-  username          = "admin"
-  password          = "Aviatrix123#"
-  save              = true
-  number_of_retries = 20
-  retry_interval    = 60
-}
-*/
-/*
-data "aws_subnet" "fwMGMTa" {
-  vpc_id = aviatrix_vpc.aws_transit.vpc_id
-  
-  filter {
-    name = "tag:Name"
-    values = ["*mgmt*-1a"]
-  }
-
-  
-}
-
-data "aws_subnet" "fwMGMTb" {
-  vpc_id = aviatrix_vpc.aws_transit.vpc_id
-  
-  filter {
-    name = "tag:Name"
-    values = ["*mgmt*-1b"]
-  }
-
-  
-}
-
-data "aws_subnet" "fwEgressa" {
-  vpc_id = aviatrix_vpc.aws_transit.vpc_id
-  
-  filter {
-    name = "tag:Name"
-    values = ["*egress*-1a"]
-  }
-
-  
-}
-
-data "aws_subnet" "fwEgressb" {
-  vpc_id = aviatrix_vpc.aws_transit.vpc_id
-  
-  filter {
-    name = "tag:Name"
-    values = ["*egress*-1b"]
-  }
-
-
-}
-
-dynamic firewall_instance_association {
-    for_each = var.firewall_names
-
-    content {
-      firenet_gw_name      = var.firenet_gw_name
-      vendor_type          = "Generic"
-      instance_id          = aviatrix_firewall_instance.firewall_instance[firewall_instance_association.value].instance_id
-      firewall_name        = aviatrix_firewall_instance.firewall_instance[firewall_instance_association.value].firewall_name
-      attached             = true
-      lan_interface        = aviatrix_firewall_instance.firewall_instance[firewall_instance_association.value].lan_interface
-      management_interface = var.cloud == "aws" ? aviatrix_firewall_instance.firewall_instance[firewall_instance_association.value].management_interface : null
-      egress_interface     = aviatrix_firewall_instance.firewall_instance[firewall_instance_association.value].egress_interface
-    }
-  }
-
-  dynamic firewall_instance_association {
-    for_each = var.firewall_names
-    #for_each = var.avtx_gw_ha ? var.firewall_names : []
-
-    content {
-      firenet_gw_name      = "${var.firenet_gw_name}-hagw"
-      vendor_type          = "Generic"
-      instance_id          = aviatrix_firewall_instance.firewall_instance_ha[firewall_instance_association.value].instance_id
-      firewall_name        = aviatrix_firewall_instance.firewall_instance_ha[firewall_instance_association.value].firewall_name
-      attached             = true
-      lan_interface        = aviatrix_firewall_instance.firewall_instance_ha[firewall_instance_association.value].lan_interface
-      management_interface = var.cloud == "aws" ? aviatrix_firewall_instance.firewall_instance_ha[firewall_instance_association.value].management_interface : null
-      egress_interface     = aviatrix_firewall_instance.firewall_instance_ha[firewall_instance_association.value].egress_interface
-    }
-  }
-*/
